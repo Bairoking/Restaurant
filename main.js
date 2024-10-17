3485ac0288dd4225036320aede454172757e50df
Bairoking <gs8004rtj5jj5j@gmail.com>
2024-10-17 10:24:34 -0700

    My first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const menuBtn = document.getElementById('menu-btn');
+const navLinks = document.getElementById('nav-links');
+const menuBtnIcon = menuBtn.querySelector('i');
+
+menuBtn.addEventListener('click', (e) => {
+    navLinks.classList.toggle('open');
+    menuBtnIcon.setAttribute('class', isOpen ? 'ri-close-line' : 'ri-menu-line');
+});
+
+navLinks.addEventListener('click', (e) => {
+    navLinks.classList.remove('open');
+    menuBtnIcon.setAttribute('class', 'ri-menu-line');
+});
+
+const scrollRevealOption = {
+    distance: '50px',
+    origin: 'bottom',
+    duration: '1000',
+};
+
+ScrollReveal().reveal('.header_image img', {
+    ...scrollRevealOption,
+    origin: 'right',
+});
+
+ScrollReveal().reveal('.header_content h2', {
+    ...scrollRevealOption,
+    delay: 500,
+});
+
+ScrollReveal().reveal('.header_content h1', {
+    ...scrollRevealOption,
+    delay: 1000,
+});
+
+ScrollReveal().reveal('.order_card', {
+    ...scrollRevealOption,
+    interval: 500,
+});
+
+ScrollReveal().reveal('.event_content', {
+    duration: '1000',
+});
